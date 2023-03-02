40a65f20667c2eb45c530d9190c9b48a80ff891a
Changhyun <chang.bae@flatironschool.com>
2023-03-01 21:29:48 -0800

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const names = [];
+function writeCards(name){
+    for (let i = 0; i < name.length; i++){
+       names.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return names;
+}
+function countDown(number){
+    while (number>-1){
+        console.log(number--);
+    }
+}
